b25e3a2ba37fc38cc8bc8bb88336ce4d69197ce6
RaushanKumar25 <raushankumarbhardwaj4510@gmail.com>
2025-10-12 17:55:09 +0530

    chnage the js code of todo app

--- Projects/2 todoApp/ToDo/ToDo.js
+++ Projects/2 todoApp/ToDo/ToDo.js
@@ -2,15 +2,20 @@ let todoList = [];
 
 function addTodo() {
     let inputElement = document.querySelector("#todo-input");
-    let todoItem = inputElement.value;
-    console.log(todoItem);
-    todoList.push(todoitem);
-    inputElement.value = "";
+    let todoItem = inputElement.value.trim();
+
+    if (todoItem !== "") {
+        todoList.push(todoItem);
+        inputElement.value = "";
+        displayItems();
+    }
 }
 
 function displayItems() {
     let displayElement = document.querySelector("#todoItems");
+    displayElement.innerText = "";
+
     for (let i = 0; i < todoList.length; i++) {
-        displayElement.innerText = displayElement.innerText + todoList[i];
-    } 
+        displayElement.innerText += (i + 1) + ". " + todoList[i] + "\n";
+    }
 }
